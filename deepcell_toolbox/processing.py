5be60cfcfef1f46304364e46cb61160959a435b2
William Graf <7930703+willgraf@users.noreply.github.com>
2020-08-26 10:52:08 -0700

    update out_range to prevent negative values passed to CLAHE

--- deepcell_toolbox/processing.py
+++ deepcell_toolbox/processing.py
@@ -75,8 +75,8 @@ def histogram_normalization(image, kernel_size=64):
     for batch in range(image.shape[0]):
         for channel in range(image.shape[-1]):
             X = image[batch, ..., channel]
-            X = rescale_intensity(X, out_range='float')
             X = equalize_adapthist(X, kernel_size=(kernel_size, kernel_size))
+            X = rescale_intensity(X, out_range=(0.0, 1.0))
             image[batch, ..., channel] = X
     return image
 
