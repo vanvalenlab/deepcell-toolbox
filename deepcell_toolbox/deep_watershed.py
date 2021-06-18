3ac3824460c22e4680bdd9c9667d32ceca17b74f
William Graf <7930703+willgraf@users.noreply.github.com>
2021-06-18 13:22:19 -0700

    Missing warnings import.

--- deepcell_toolbox/deep_watershed.py
+++ deepcell_toolbox/deep_watershed.py
@@ -28,6 +28,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import warnings
+
 import numpy as np
 import scipy.ndimage as nd
 
