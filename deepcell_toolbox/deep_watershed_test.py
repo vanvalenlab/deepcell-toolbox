bb240e04ffaede1b311c4f42df4e9eacbb5c8520
willgraf <7930703+willgraf@users.noreply.github.com>
2021-06-22 16:32:07 -0700

    Combine `deep_watershed` and `deep_watershed_mibi` (#108)
    * Deprecate several arguments in favor of the `deep_watershed_mibi` arguments.
    
    * New `maxima_algorithm` to support switching between `h_maxima` and `peak_local_max`
    
    * Allow 3D to use `h_maxima` with `ball`.
    
    * Warn user that `fill_holes` is not supported in 3D.
    
    `deep_watershed_3D` is totally deprecated in favor of `deep_watershed`. 
    
    `deep_watershed_mibi` still performs some input validation and reformats the input for `deep_watershed.

--- deepcell_toolbox/deep_watershed_test.py
+++ deepcell_toolbox/deep_watershed_test.py
@@ -35,29 +35,75 @@ import pytest
 from deepcell_toolbox import deep_watershed
 
 
-def _get_image(img_h=300, img_w=300):
-    bias = np.random.rand(img_w, img_h) * 64
-    variance = np.random.rand(img_w, img_h) * (255 - 64)
-    img = np.random.rand(img_w, img_h) * variance + bias
-    return img
-
-
 def test_deep_watershed():
     shape = (5, 21, 21, 1)
-    inner_distance = np.random.random(shape) * 100
-    outer_distance = np.random.random(shape) * 100
-    fgbg = np.random.randint(0, 1, size=shape)
-    inputs = [inner_distance, outer_distance, fgbg]
+    maxima = np.random.random(shape) * 100
+    interior = np.random.random(shape) * 100
+    other = np.random.randint(0, 1, size=shape)
+    inputs = [maxima, interior]
 
-    # basic tests
-    watershed_img = deep_watershed.deep_watershed(inputs)
-    np.testing.assert_equal(watershed_img.shape, shape[:-1])
+    # basic tests for both h_maxima and peak_local_max
+    for algo in ('h_maxima', 'peak_local_max'):
+        label_img = deep_watershed.deep_watershed(inputs, maxima_algorithm=algo)
+        np.testing.assert_equal(label_img.shape, shape[:-1] + (1,))
 
-    # turn some knobs
-    watershed_img = deep_watershed.deep_watershed(inputs,
-                                                  small_objects_threshold=1,
-                                                  exclude_border=True)
-    np.testing.assert_equal(watershed_img.shape, shape[:-1])
+        # flip the order and give correct indices, same answer
+        label_img_2 = deep_watershed.deep_watershed([other, maxima, interior],
+                                                    maxima_index=1,
+                                                    interior_index=2,
+                                                    maxima_algorithm=algo)
+        np.testing.assert_array_equal(label_img, label_img_2)
+
+        # all the bells and whistles
+        label_img_3 = deep_watershed.deep_watershed(inputs, maxima_algorithm=algo,
+                                                    small_objects_threshold=1,
+                                                    label_erosion=1,
+                                                    pixel_expansion=1,
+                                                    fill_holes_threshold=1)
+
+        np.testing.assert_equal(label_img_3.shape, shape[:-1] + (1,))
+
+    # test bad inputs, pairs of maxima and interior shapes
+    bad_shapes = [
+        ((1, 32, 32, 1), (1, 32, 16, 1)),  # unequal dimensions
+        ((1, 32, 32, 1), (1, 16, 32, 1)),  # unequal dimensions
+        ((32, 32, 1), (32, 32, 1)),  # no batch dimension
+        ((1, 32, 32), (1, 32, 32)),  # no channel dimension
+        ((1, 5, 10, 32, 32, 1), (1, 5, 10, 32, 32, 1)),  # too many dims
+    ]
+    for bad_maxima_shape, bad_interior_shape in bad_shapes:
+        bad_inputs = [np.random.random(bad_maxima_shape),
+                      np.random.random(bad_interior_shape)]
+        with pytest.raises(ValueError):
+            deep_watershed.deep_watershed(bad_inputs)
+
+    # test bad values of maxima_algorithm.
+    with pytest.raises(ValueError):
+        deep_watershed.deep_watershed(inputs, maxima_algorithm='invalid')
+
+    # pass weird data types
+    bad_inputs = [
+        {'interior-distance': maxima, 'outer-distance': interior},
+        None,
+    ]
+    for bad_input in bad_inputs:
+        with pytest.raises(ValueError):
+            _ = deep_watershed.deep_watershed(bad_input)
+
+    # test deprecated values still work
+    # each pair is the deprecated name, then the new name.
+    old_new_pairs = [
+        ('min_distance', 'radius', np.random.randint(10)),
+        ('distance_threshold', 'interior_threshold', np.random.randint(1, 100) / 100),
+        ('detection_threshold', 'maxima_threshold', np.random.randint(1, 100) / 100),
+    ]
+    for deprecated_arg, new_arg, value in old_new_pairs:
+        dep_kwargs = {deprecated_arg: value}
+        new_kwargs = {new_arg: value}
+
+        dep_img = deep_watershed.deep_watershed(inputs, **dep_kwargs)
+        new_img = deep_watershed.deep_watershed(inputs, **new_kwargs)
+        np.testing.assert_array_equal(dep_img, new_img)
 
 
 def test_deep_watershed_mibi():
@@ -106,17 +152,33 @@ def test_deep_watershed_mibi():
 
 def test_deep_watershed_3D():
     shape = (5, 10, 21, 21, 1)
-    inner_distance = np.random.random(shape) * 100
-    outer_distance = np.random.random(shape) * 100
-    fgbg = np.random.randint(0, 1, size=shape)
-    inputs = [inner_distance, outer_distance, fgbg]
+    maxima = np.random.random(shape) * 100
+    interior = np.random.random(shape) * 100
+    other = np.random.randint(0, 1, size=shape)
+    inputs = [maxima, interior]
 
-    # basic tests
-    watershed_img = deep_watershed.deep_watershed_3D(inputs)
-    np.testing.assert_equal(watershed_img.shape, shape[:-1])
+    # basic tests for both h_maxima and peak_local_max
+    for algo in ('h_maxima', 'peak_local_max'):
+        label_img = deep_watershed.deep_watershed(inputs, maxima_algorithm=algo)
+        np.testing.assert_equal(label_img.shape, shape[:-1] + (1,))
 
-    # turn some knobs
-    watershed_img = deep_watershed.deep_watershed_3D(inputs,
-                                                     small_objects_threshold=1,
-                                                     exclude_border=True)
-    np.testing.assert_equal(watershed_img.shape, shape[:-1])
+        # flip the order and give correct indices, same answer
+        label_img_2 = deep_watershed.deep_watershed([other, maxima, interior],
+                                                    maxima_index=1,
+                                                    interior_index=2,
+                                                    maxima_algorithm=algo)
+        np.testing.assert_array_equal(label_img, label_img_2)
+
+        # all the bells and whistles
+        label_img_3 = deep_watershed.deep_watershed(inputs, maxima_algorithm=algo,
+                                                    small_objects_threshold=1,
+                                                    label_erosion=1,
+                                                    pixel_expansion=1,
+                                                    fill_holes_threshold=1)
+
+        np.testing.assert_equal(label_img_3.shape, shape[:-1] + (1,))
+
+        # test deprecated `deep_watershed_3D` function
+        label_img_3d = deep_watershed.deep_watershed_3D(
+            inputs, maxima_algorithm=algo)
+        np.testing.assert_array_equal(label_img, label_img_3d)
